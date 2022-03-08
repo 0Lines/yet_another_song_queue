7af6c65a05f1aae1573e4c0b4b2e7ae686975709
GuilhermePaini <guihdpaini@gmail.com>
2022-03-08 00:12:37 -0300

    added "no songs" card on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,15 +13,31 @@
                         dense
                         height="20px"
                         v-model="songUrl"
-                        @keyup.enter="validateAndSong"
+                        @keyup.enter="validateAndAddSong"
                     ></v-text-field>
                 </v-sheet>
                 <v-container class="round-container pa-0 my-5">
                     <PlayingSong />
                 </v-container>
-                <v-list class="round-container">
-                    <SongListCard v-for="(song, n) in songs" :key="n" :index="n" :song="song" @deleteSong="remove"/>
+                <v-list v-if="songs.length > 0" class="round-container">
+                    <SongListCard 
+                        v-for="(song, n) in songs" 
+                        :key="n" 
+                        :index="n" 
+                        :song="song" 
+                        @deleteSong="remove"
+                    />
                 </v-list>
+                <v-container v-else class="round-container pa-0 my-5">
+                    <v-card elevation="0">
+                        <v-card-title class="justify-center">
+                            <span>Your playlist is empty!🤠</span>
+                        </v-card-title>
+                        <v-card-text class="text-center">
+                            Add songs by pasting the youtube or spotify link into the search bar. 🔥
+                        </v-card-text>
+                    </v-card>
+                </v-container>
             </v-col>
         </v-row>
     </v-container>
@@ -30,17 +46,14 @@
 <script>
 import SongListCard from "@/components/SongListCard.vue"
 import PlayingSong from "@/components/PlayingSong.vue"
+import ytdl from "ytdl-core"
 
 export default {
     props: {},
     mixins: {},
     data(){
         return {
-            songs: [
-                {name: "bu", artist: "oia"},
-                {name: "asdasdsa", artist: "lelele"},
-                {name: "caraio", artist: "oloco"},
-            ],
+            songs: [],
             songUrl: "",
         }
     },
@@ -52,8 +65,9 @@ export default {
     computed: {},
     watch: {},
     methods: {
-        validateAndAddSong() {
-
+        async validateAndAddSong() {
+            const info = await ytdl.getInfo(this.songUrl);
+            console.log(info);
         },
         remove(index) {
             this.songs.splice(index, 1);
