af84a33e9ab3311b69fcf6f201376bf4ca0ade31
Arthur <arthurbziero@gmail.com>
2022-04-03 12:13:53 -0300

    add song control buttons

--- src/components/PlayingSong.vue
+++ src/components/PlayingSong.vue
@@ -1,15 +1,45 @@
 <template>
     <v-card flat>
-        <div class="d-flex">
-            <v-avatar class="ma-3" size="125" tile>
-                <v-img :src="song.thumbnails[0].url"></v-img>
+        <v-row class="ma-2">
+            <v-avatar class="ma-3" size="125">
+                <v-img :class="imageClass" :src="song.thumbnails[0].url" />
             </v-avatar>
-			
-            <div>
-                <v-card-title class="text-h5">{{ song.title }}</v-card-title>
-                <v-card-subtitle>{{ song.ownerChannelName }}</v-card-subtitle>
-            </div>
-        </div>
+		
+            <v-col>	
+                <div>
+                    <v-card-title class="pl-0 pt-0" style="overflow: hidden; text-overflow: ellipsis;">
+                        <v-icon color="secondary" class="mr-2"> mdi-music </v-icon>
+                        {{ song.title }}
+                    </v-card-title>
+                    <v-card-subtitle class="pl-0 pt-0">
+                        <v-icon color="secondary" class="mr-2"> mdi-account-music </v-icon>
+                        {{ song.ownerChannelName }}
+                    </v-card-subtitle>
+                </div>
+
+                <v-sheet class="d-flex justify-center">
+                    <v-btn
+                        icon
+                        x-large
+                        class="mr-2">
+                        <v-icon size="54" color="secondary"> mdi-skip-previous </v-icon>
+                    </v-btn>
+                    <v-btn
+                        icon
+                        x-large
+                        @click="handleClick"
+                        class="mr-2">
+                        <v-icon size="58" color="secondary"> {{ playButtonIcon }} </v-icon>
+                    </v-btn>
+                    <v-btn
+                        icon
+                        x-large
+                        class="mr-2">
+                        <v-icon size="54" color="secondary"> mdi-skip-next </v-icon>
+                    </v-btn>
+                </v-sheet>
+            </v-col>	
+        </v-row>
     </v-card>
 </template>
 
@@ -25,15 +55,43 @@ export default {
     mixins: {},
     data(){
         return {
+            isPlaying: true,
         }
     },
     directives: {},
     components: {},
-    computed: {},
+    computed: {
+        playButtonIcon() {
+            return this.isPlaying ? 'mdi-pause-circle' : 'mdi-play-circle'
+        },
+        imageClass() {
+            return this.isPlaying ? 'spin' : ''
+        },
+    },
     watch: {},
-    methods: {},
+    methods: {
+        handleClick() {
+            this.isPlaying ? this.stop() : this.play()
+        },
+        play() {
+            this.isPlaying = true
+        },
+        stop() {
+            this.isPlaying = false
+        }
+    },
 }
 </script>
 
 <style>
+    @keyframes spinning {
+        from { transform: rotate(0deg) }
+        to { transform: rotate(360deg) }
+    }
+    .spin {
+        animation-name: spinning;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+    }
 </style>
